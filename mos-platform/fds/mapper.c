259f883b4d7e8ad6f773431591ec778aef24da17
Essem <smswessem@gmail.com>
2025-10-07 19:17:15 -0500

    fds: More basic mapper stuff

--- mos-platform/fds/mapper.c
+++ mos-platform/fds/mapper.c
@@ -3,7 +3,15 @@
 #define FDS_CTRL 0x4025
 #define FDS_CTRL_MIRROR 0xFA
 
-__attribute__((weak)) void set_fds_ctrl(char value) {
+#define FDS_EXT 0x4026
+#define FDS_EXT_MIRROR 0xF9
+
+__attribute__((leaf)) void set_fds_ctrl(char value) {
   POKE(FDS_CTRL_MIRROR, value);
   POKE(FDS_CTRL, value);
+}
+
+__attribute__((leaf)) void write_ext_data(char value) {
+  POKE(FDS_EXT_MIRROR, value);
+  POKE(FDS_EXT, value);
 }
--- mos-platform/fds/mapper.h
+++ mos-platform/fds/mapper.h
@@ -8,6 +8,18 @@ extern "C" {
 // Set all 8 bits of the $4025 FDS Control register.
 __attribute__((leaf)) void set_fds_ctrl(char value);
 
+// Write 7-bit data to the rear expansion port ($4026).
+__attribute__((leaf)) void write_ext_data(char value);
+
+struct __fds_read {
+    unsigned char disk_status;
+    unsigned char data;
+    unsigned char drive_status;
+    unsigned char ext_data;
+};
+
+#define FDS_READ (*(volatile struct __fds_read*)0x4030)
+
 #define MIRROR_VERTICAL 0
 #define MIRROR_HORIZONTAL 1
 
